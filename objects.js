c0f2b200959854d1342ec9e8b2cc78a58ccd8dc9
Aubree Abril <aubree.abril@gmail.com>
2017-12-13 04:10:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName
-  playlist.songTitle
+  playlist.artistName = artistName
+  playlist.songTitle = songTitle
   return playlist
 }
